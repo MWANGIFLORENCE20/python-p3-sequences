ff823da2d1caa0d3e879b1e6755c78f2ad39adda
florence wangechi <florencewangechim@gmail.com>
2023-05-18 10:39:19 +0300

    Completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(lenght):
+    fib_list = []
+    a,b = 0,1
+
+    for _ in range(lenght):
+        fib_list.append(a)
+        a,b = b, a + b
+
+
+    print(fib_list)  
